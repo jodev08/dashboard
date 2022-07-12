189dbd07e031bdfb17e2af14b979edf800d145b8
jodev08 <j.stonskadev@gmail.com>
2022-07-12 14:22:38 +0200

    navbar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Sidebar.css';
+
+import { SideBarData } from '../../Data/Data';
+
+import {UilSignOutAlt} from "@iconscout/react-unicons";
+
+
+
+
+const Sidebar = () => {
+ 
+
+  
+  return (
+    <div className='Sidebar '>
+        
+       
+         
+         {/*menu nav */}
+          <div className='navigation '>
+            {SideBarData.map((item,index) =>{
+              return(
+              
+              
+              <div className='navigationItem'>
+               
+                <item.icon></item.icon>
+                <span>
+                  {item.heading}
+                </span>
+
+              </div>
+              
+              )
+            })}
+            
+            <div className='switchOff'>
+            <UilSignOutAlt></UilSignOutAlt>
+            </div>
+
+            
+            
+          </div>
+   
+        
+    </div>
+
+          
+
+        
+
+ 
+  
+
+
+  )
+}
+
+export default Sidebar;
